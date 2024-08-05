2363ff3667697f401644f904285c71c88925497f
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-08-05 11:37:16 -0400

    Initial commit

---
+++ bin/concatenate.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+
+import json
+from argparse import ArgumentParser
+from collections import defaultdict
+from datetime import datetime
+from os import fspath, walk, listdir
+from pathlib import Path
+from typing import Dict, Tuple
+
+import anndata
+import muon as mu
+import numpy as np
+import pandas as pd
+import requests
+import scipy.sparse
+import uuid
+import yaml
+
+GENE_MAPPING_DIRECTORIES = [
+    Path(__file__).parent.parent / "data",
+    Path("/opt/data"),
+]
+
+
+def get_tissue_type(dataset: str) -> str:
+    organ_dict = yaml.load(open("/opt/organ_types.yaml"), Loader=yaml.BaseLoader)
+    organ_code = requests.get(
+        f"https://entity.api.hubmapconsortium.org/dataset/{dataset}/organs/"
+    )
+    organ_name = organ_dict[organ_code]
+    return organ_name.replace(" (Left)", "").replace(" (Right)", "")
+
+
+def convert_tissue_code(tissue_code):
+    with open("/opt/organ_types.yaml", 'r') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    tissue_name = data.get(tissue_code)['description']
+    return tissue_name
+
+
+def get_inverted_gene_dict():
+    inverted_dict = defaultdict(list)
+    gene_mapping = read_gene_mapping()
+    for ensembl, hugo in gene_mapping.items():
+        inverted_dict[hugo].append(ensembl)
+    return inverted_dict
+
+
+def find_files(directory, patterns):
+    for dirpath_str, dirnames, filenames in walk(directory):
+        dirpath = Path(dirpath_str)
+        for filename in filenames:
+            filepath = dirpath / filename
+            for pattern in patterns:
+                if filepath.match(pattern):
+                    return filepath
+                
+
+def find_file_pairs(directory):
+    cell_by_bin_patterns = ["cell_by_bin.h5ad"]
+    cell_by_gene_patterns = ["cell_by_gene.h5ad"]
+    cell_by_bin_file = find_files(directory, cell_by_bin_patterns)
+    cell_by_gene_file = find_files(directory, cell_by_gene_patterns)
+    return (cell_by_bin_file, cell_by_gene_file)
+
+
+def annotate_h5ads(
+    adata_file, tissue_type: str, uuids_df: pd.DataFrame):
+    # Get the directory
+    data_set_dir = fspath(adata_file.parent.stem)
+    # And the tissue type
+    tissue_type = tissue_type if tissue_type else get_tissue_type(data_set_dir)
+    hubmap_id = uuids_df.loc[uuids_df["uuid"] == data_set_dir, "hubmap_id"].values[0]
+    adata = anndata.read_h5ad(adata_file)
+    adata_copy = adata.copy()
+    adata_copy.obs["barcode"] = adata.obs.index
+    adata_copy.obs["dataset"] = data_set_dir
+    adata_copy.obs["hubmap_id"] = hubmap_id
+    adata_copy.obs["organ"] = tissue_type
+    adata_copy.obs["modality"] = "atac"
+    
+    cell_ids_list = [
+        "-".join([data_set_dir, barcode]) for barcode in adata_copy.obs["barcode"]
+    ]
+    adata_copy.obs["cell_id"] = pd.Series(
+        cell_ids_list, index=adata_copy.obs.index, dtype=str
+    )
+    adata_copy.obs.set_index("cell_id", drop=True, inplace=True)
+    adata_copy = map_gene_ids(adata_copy)
+    return adata_copy
+
+
+def read_gene_mapping() -> Dict[str, str]:
+    """
+    Try to find the Ensembl to HUGO symbol mapping, with paths suitable
+    for running this script inside and outside a Docker container.
+    :return:
+    """
+    for directory in GENE_MAPPING_DIRECTORIES:
+        mapping_file = directory / "ensembl_to_symbol.json"
+        if mapping_file.is_file():
+            with open(mapping_file) as f:
+                return json.load(f)
+    message_pieces = ["Couldn't find Ensembl → HUGO mapping file. Tried:"]
+    message_pieces.extend(f"\t{path}" for path in GENE_MAPPING_DIRECTORIES)
+    raise ValueError("\n".join(message_pieces))
+
+
+def map_gene_ids(adata):
+    obsm = adata.obsm
+    uns = adata.uns
+    gene_mapping = read_gene_mapping()
+    has_hugo_symbol = [gene in gene_mapping for gene in adata.var.index]
+    # adata = adata[:, has_hugo_symbol]
+    temp_df = pd.DataFrame(
+        adata.X.todense(), index=adata.obs.index, columns=adata.var.index
+    )
+    aggregated = temp_df.groupby(level=0, axis=1).sum()
+    adata = anndata.AnnData(aggregated, obs=adata.obs)
+    adata.var["hugo_symbol"] = [
+        gene_mapping.get(var, np.nan) for var in adata.var.index
+    ]
+    adata.obsm = obsm
+    adata.uns = uns
+    # This introduces duplicate gene names, use Pandas for aggregation
+    # since anndata doesn't have that functionality
+    adata.X = scipy.sparse.csr_matrix(adata.X)
+    adata.var_names_make_unique()
+    return adata
+
+
+def create_json(tissue, data_product_uuid, creation_time, uuids, hbmids, cell_count):
+    bucket_url = f"https://hubmap-data-products.s3.amazonaws.com/{data_product_uuid}/"
+    metadata = {
+        "Data Product UUID": data_product_uuid,
+        "Tissue": convert_tissue_code(tissue),
+        "Raw URL": bucket_url + f"{tissue}_raw.h5ad",
+        "Processed URL": bucket_url + f"{tissue}_processed.h5ad",
+        "Creation Time": creation_time,
+        "Dataset UUIDs": uuids,
+        "Dataset HBMIDs": hbmids,
+        "Total Cell Count": cell_count
+    }
+    print("Writing metadata json")
+    with open(f"{data_product_uuid}.json", "w") as outfile:
+        json.dump(metadata, outfile)
+
+
+def make_mudata(cell_by_bin, cell_by_gene):
+    mdata = mu.MuData({"atac_cell_by_bin": cell_by_bin, "atac_cell_by_gene": cell_by_gene})
+    mu.pp.intersect_obs
+    return mdata
+
+
+def annotate_mudata(mdata, uuids_df):
+    pass
+
+
+
+def main(data_directory: Path, uuids_file: Path, tissue: str = None):
+    output_file_name = f"{tissue}" if tissue else "atac"
+    uuids_df = pd.read_csv(uuids_file, sep="\t", dtype=str)
+    uuids_list = uuids_df["uuid"].to_list()
+    hbmids_list = uuids_df["hubmap_id"].to_list()
+    directories = [data_directory / Path(uuid) for uuid in uuids_df["uuid"]]
+    # Load files
+    file_pairs = [find_file_pairs(directory) for directory in directories if len(listdir(directory))>1]
+    print("Annotating objects")
+    cell_by_bin_adatas = [
+        annotate_h5ads(file_pair[0], file_pair[1], tissue, uuids_df)
+        for file_pair in file_pairs
+    ]
+
+    cell_by_gene_adatas = [     
+        annotate_h5ads(file_pair[0], file_pair[1], tissue, uuids_df)
+        for file_pair in file_pairs
+    ]
+    saved_var = cell_by_gene_adatas[0].var
+    print("Concatenating objects")
+    cbb_concat = anndata.concat(cell_by_bin_adatas, join="outer")
+    cbg_concat = anndata.concat(cell_by_gene_adatas, join="outer")
+    creation_time = str(datetime.now())
+    cbb_concat.uns["creation_date_time"] = cbg_concat.uns["creation_date_time"] = creation_time
+    cbb_concat.uns["datasets"] = cbg_concat.uns["datasets"] = hbmids_list
+    data_product_uuid = str(uuid.uuid4())
+    cbb_concat.uns["uuid"] = cbg_concat.uns["uuid"] = data_product_uuid
+    cbb_concat.var = cbg_concat.var = saved_var
+    total_cell_count = cbb_concat.obs.shape[0]
+    mdata = make_mudata(cbb_concat, cbg_concat)
+    mdata.write(f"{output_file_name}.h5ad")
+    create_json(tissue, data_product_uuid, creation_time, uuids_list, hbmids_list, total_cell_count)
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("data_directory", type=Path)
+    p.add_argument("uuids_file", type=Path)
+    p.add_argument("tissue", type=str, nargs="?")
+    p.add_argument("--enable_manhole", action="store_true")
+
+    args = p.parse_args()
+
+    if args.enable_manhole:
+        import manhole
+
+        manhole.install(activate_on="USR1")
+
+    main(args.data_directory, args.uuids_file, args.tissue)
---
+++ make_uuids_tsv.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+import json
+from argparse import ArgumentParser
+
+import pandas as pd
+import requests
+import yaml
+
+organ_types_yaml_file = "bin/organ_types.yaml"
+
+
+def extract_donor_metadata(metadata):
+    donor_info = {
+        "age": None,
+        "sex": None,
+        "height": None,
+        "weight": None,
+        "bmi": None,
+        "cause_of_death": None,
+        "race": None 
+    }
+
+    for item in metadata.get('organ_donor_data', []):
+        concept = item.get('grouping_concept_preferred_term')
+        value = item.get('data_value')
+        if concept == "Age":
+            donor_info["age"] = value
+        elif concept == "Sex":
+            donor_info["sex"] = item.get('preferred_term')
+        elif concept == "Height":
+            donor_info["height"] = value
+        elif concept == "Weight":
+            donor_info["weight"] = value
+        elif concept == "Body mass index":
+            donor_info["bmi"] = value
+        elif concept == "Cause of death":
+            donor_info["cause_of_death"] = item.get('preferred_term')
+        elif concept == "Race":
+            donor_info["race"] = item.get('preferred_term')
+
+    return donor_info
+
+
+def get_uuids(organ_name_mapping: dict, organ: str = None):
+    organ_code_mapping = {organ_name_mapping[key]: key for key in organ_name_mapping}
+    if organ == None:
+        query_payload = {
+            "from": 0,
+            "size": 10000,
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"dataset_type": "ATACseq"}},
+                        {"match": {"data_access_level": "public"}},
+                    ]
+                }
+            },
+        }
+    else:
+        query_payload = {
+            "from": 0,
+            "size": 10000,
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"dataset_type": "ATACseq"}},
+                        {"match": {"data_access_level": "public"}},
+                        {"match": {"origin_samples.organ": organ_code_mapping[organ]}},
+                    ]
+                }
+            },
+        }
+    query_payload_str = json.dumps(query_payload)
+    print(query_payload_str)
+
+    url = "https://search.api.hubmapconsortium.org/v3/search"
+    response = requests.post(url, json=query_payload)
+    print("Response status code: ", response.status_code)
+    print("Response url: ", response.url)
+    print("Response reason: ", response.reason)
+    if response.status_code == 200:
+        data = response.json()
+        # Accessing nested dictionaries to retrieve uuid and hubmap_id
+        hits = data.get("hits", {}).get("hits", [])
+        uuids = []
+        hubmap_ids = []
+        donor_metadata_list = []
+
+        for hit in hits:
+            source = hit["_source"]
+            uuids.append(source["uuid"])
+            hubmap_ids.append(source["hubmap_id"])
+            donor_metadata = source.get("donor", {}).get("metadata", {})
+            donor_metadata_list.append(extract_donor_metadata(donor_metadata))
+
+        return uuids, hubmap_ids, donor_metadata_list
+    
+    elif response.status_code == 303:
+        print("Response body: ", response.text)
+        redirection_url = response.text
+        print("Redirection URL: ", redirection_url)
+        redirection_response = requests.get(redirection_url)
+        if redirection_response.status_code == 200:
+            data = redirection_response.json()
+            hits = data.get("hits", {}).get("hits", [])
+            uuids = [hit["_source"]["uuid"] for hit in hits]
+            hubmap_ids = [hit["_source"]["hubmap_id"] for hit in hits]
+            return uuids, hubmap_ids
+    else:
+        print("Error:", response.status_code)
+        return [], [], []
+
+
+def main(tissue_type: str):
+    organ_dict = yaml.load(open(organ_types_yaml_file), Loader=yaml.BaseLoader)
+    for key in organ_dict:
+        organ_dict[key] = organ_dict[key]["description"]
+    if tissue_type not in organ_dict.values():
+        print(f"Tissue {tissue_type} not found ")
+        tissue_type = None
+    uuids_list, hubmap_ids_list, donor_metadata = get_uuids(organ_dict, tissue_type)
+    uuids_df = pd.DataFrame()
+    uuids_df["uuid"] = pd.Series(uuids_list, dtype=str)
+    uuids_df["hubmap_id"] = pd.Series(hubmap_ids_list, dtype=str)
+    donor_metadata_df = pd.DataFrame(donor_metadata)
+    result_df = pd.concat([uuids_df, donor_metadata_df], axis=1)
+    key_for_tissue = [key for key, value in organ_dict.items() if value == tissue_type]
+    if key_for_tissue:
+            output_file_name = f"{key_for_tissue[0].lower()}.tsv"
+    else:
+        output_file_name = "atac.tsv"
+    print(result_df)
+    result_df.to_csv(output_file_name, sep="\t")
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("tissue_type", type=str, nargs="?", help="Type of tissue (optional)")
+
+    args = p.parse_args()
+
+    main(args.tissue_type)
